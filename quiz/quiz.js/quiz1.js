71a362ea61a3dd2c19196a6943ef93444ee9469b
Muriel MARCHAL <muriel.marchal@hotmail.com>
2023-07-27 08:52:29 +0200

    modif quiz add questions in js

--- quiz/quiz.js/quiz1.js
+++ quiz/quiz.js/quiz1.js
@@ -1,2 +1,112 @@
-const question1 = document.getElementById(quiz_click1);
-console.log (question1);
+let questions = [
+    {
+        numb: 1,
+        question: "Quels sont les prénoms des enfants de Ray et Debra ?",
+        answer: "Ally, Jeffrey et Michael",
+        options: [
+            "Albert, Jeffrey et Marie",
+            "Ally, Jeffrey et Michael",
+            "Ally, John et Marc",
+            "Marie, Frank et Robert"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quel est le métier de Ray ?",
+        answer: "journaliste sportif",
+        options: [
+            "journaliste sportif",
+            "joueur de golf",
+            "critique littéraire",
+            "père au foyer"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Comment Ray et Debra se sont-ils rencontrés ?",
+        answer: "Ray a livré un canapé chez Debra en compagnie de son copain Gianni",
+        options: [
+            "à l'université, dans un cours de psychologie",
+            "à l'église",
+            "Ray a livré un canapé chez Debra en compagnie de son copain Gianni",
+            "Robert a arrêté Debra pour excès de vitesse"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quelle est l'étrange manie de Robert ?",
+        answer: "il touche son menton avec sa nourriture avant de la manger",
+        options: [
+            "il touche son menton avec sa nourriture avant de la manger",
+            "il met tous les jours la même paire de chausettes",
+            "ich chante l'Avé Maria avant de rentrer chez ses parents",
+            "il fait trois fois le tour de la maison chaque matin"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Qu'aime faire Frank quand il vient chez Ray et Debra",
+        answer: "regarder la télévision",
+        options: [
+            "manger les bons petits plats de Debra",
+            "faire sa lessive",
+            "jouer avec ses petits enfants",
+            "regarder la télévision"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Qu'aime faire Marie quand elle vient chez Ray et Debra",
+        answer: "tout à la fois",
+        options: [
+            "regarder dans le réfrigérateur et apporter des plats tout prêts",
+            "fouiller dans les placards et tiroirs de Debra",
+            "espionner le courrier",
+            "tout à la fois"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Comment s'appele le collègue et ami de Ray",
+        answer: "Andy",
+        options: [
+            "Bernie",
+            "Gianni",
+            "Frank",
+            "Andy"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Comment s'appellent les parents de Debra ?",
+        answer: "Loïs et Warren",
+        options: [
+            "Loïs et Warren",
+            "Marie et Frank",
+            "Patricia et Hank",
+            "Lee et Stan"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Dans quelle pièce de la maison Ray et Debra ne se sont jamais disputés ?",
+        answer: "en fait, aucune",
+        options: [
+            "le bureau de Ray au sous-sol",
+            "la salle de bain",
+            "le garage",
+            "en fait, aucune"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Dans quelle pièce de la maison se termine la série ?",
+        answer: "la cuisine de Ray et Debra",
+        options: [
+            "le salon de Ray et Debra",
+            "la cuisine de Marie et Frank",
+            "la cuisine de Ray et Debra",
+            "la salle de bain de Ray et Debra"
+        ]
+    },
+]
---
+++ quiz/quiz.js/quiz2.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Quels sont les prénoms des fils de Marie et Frank ?",
+        answer: "Robert et Ray",
+        options: [
+            "Ramon et Randy",
+            "Robert et Romain",
+            "Robert et Ray",
+            "Ray et Regis"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quel cadeau fait Ray à son père, que ce dernier trouve mieux que la télévision ?",
+        answer: "un aquarium avec des poissons exotiques",
+        options: [
+            "un livre sur la Corée",
+            "un aquarim avec des poissons exotiques",
+            "une photo de famille",
+            "rien n'est mieux que la télévision pour Frank"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Quels noms Frank donne-t'il à ses poissons exotiques ?",
+        answer: "Marie, Lee et Stan",
+        options: [
+            "Marie, Lee et Stan",
+            "Marie, Amy et Debra",
+            "Marie, Robert et Ray",
+            "Ally, Jeffrey et Michael"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quels sont les prénoms des enfants de Ray et Debra ?",
+        answer: "Ally, Jeffrey et Michael",
+        options: [
+            "Albert, Jeffrey et Marie",
+            "Ally, Jeffrey et Michael",
+            "Ally, John et Marc",
+            "Marie, Frank et Robert"
+        ]
+    },
+    {
+        numb: 5,
+        question: "qui sont Lee et Stan ?",
+        answer: "des amis proches de Frank et Marie",
+        options: [
+            "des amis d'université de Robert",
+            "des collègues de travail de Ray",
+            "des amis proches de Frank et Marie",
+            "en fait, on ne sait pas..."
+        ]
+    },
+    {
+        numb: 6,
+        question: "Dans la saison 4, au début de l'épisode du papier peint, comment Frank et Marie entrent chez Ray ?",
+        answer: "la voiture défonce le mur en marche arrière",
+        options: [
+            "ils arrivent par la porte d'entrée avec un cheval",
+            "la voiture défonce le mur en marche arrière",
+            "ils entrent par la porte de derrière parce que la porte d'entrée est verrouillée",
+            "ils entrent normalement sans frapper"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Dans la saison 4, où Marie emmène-t-elle sa famille en voyage ?",
+        answer: "en Italie",
+        options: [
+            "à Las Vegas",
+            "en Italie",
+            "à Hollywood",
+            "à la maison"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Au début de la saison 9, pourquoi Frank et Marie se font-ils virés de la maison de retraite ?",
+        answer: "ils sont tellement insupportables que les autres résidents ont menacé d'arrêter de prendre leur médicaments",
+        options: [
+            "ils ont oublié de payer l'adhésion",
+            "Ray trouve qu'ils habitent trop loin et ne le supporte pas",
+            "ils sont tellement insupportables que les autres résidents ont menacé d'arrêter de prendre leur médicaments",
+            "ils chantent trop fort sous la douche et leur joie de vivre déprime tout le monde"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Pourquoi y-at'il une grande fourchette et une grande cuilllière accrochées au mur de la cuisine ?",
+        answer: "Ally, Jeffrey et Michael",
+        options: [
+            "il s'agit des couverts de bébé de Robert",
+            "la décoration venait avec la maison",
+            "c'est un cadeau de mariage",
+            "en fait, on ne sait pas..."
+        ]
+    },
+    {
+        numb: 10,
+        question: "Comment s'appele le restaurant préféré de la famille Barone ?",
+        answer: "chez Nemo",
+        options: [
+            "chez Nemo",
+            "a la bonne fourchette",
+            "vivement qu'on mange",
+            "la cuisine de Marie"
+        ]
+    },
+]
